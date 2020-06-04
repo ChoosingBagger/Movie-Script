f2ee528602283a5dd44a889b409a7c08bc75feb1
MacGyver <majormacgyver@protonmail.com>
2020-06-03 23:02:29 -0500

    Added screens directory method. Split the variables into more identifiable ones.

--- folder.py
+++ folder.py
@@ -5,14 +5,20 @@ from pathlib import Path
 class Folder(object):
 
     def __init___(self):
-        self.outputDir = ""
+        self.movieDir = ""
+        self.screenDir = ""
 
     def build(self, dir, title):
         self.createMovieDir(dir, title)
+        self.createScreenDir()
 
     def createMovieDir(self, dir, title):
         dir = Path(dir)
-        self.outputDir = dir / title
-        if not os.path.exists(self.outputDir):
-            os.makedirs(self.outputDir)
-        return self.outputDir
+        self.movieDir = dir / title
+        if not os.path.exists(self.movieDir):
+            os.makedirs(self.movieDir)
+
+    def createScreenDir(self):
+        self.screenDir = self.movieDir / "screens"
+        if not os.path.exists(self.screenDir):
+            os.makedirs(self.screenDir)
